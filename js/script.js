7d3a95441172bfb2af6536ef27f00f5a5c75126c
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-07-14 21:13:22 +0400

    Adding one more section with scroll animation

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@
 
 let navTop = document.querySelector(".navTop");
 let scollUp = document.querySelector(".scrollUp");
+let hurryUpContent = document.querySelector(".hurryUpContent");
 
 window.addEventListener("scroll", () => {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         navTop.style.height = "0px";
         scollUp.style.opacity = "1";
+    }
+    else if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        hurryUpContent.style.display = "none";
     }
     else if (document.body.scrollTop < 1 || document.documentElement.scrollTop < 1) {
         navTop.style.height = "30px";
@@ -17,4 +21,10 @@ window.addEventListener("scroll", () => {
 scollUp.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", () => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        hurryUpContent.style.display = "block";
+    }
 });
